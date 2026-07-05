b32da4824d6e8dcb4162bc5dd7f6f40cfe282216
hxrshvxrdhxn <hchauhan8586@gmail.com>
2026-07-05 20:24:58 +0530

    fix: Remove unused typescript scratch files causing build errors

--- check_html2.ts
+++
@@ -1,8 +0,0 @@
-const { getPostBySlug } = require('./lib/posts');
-
-async function run() {
-  const post = await getPostBySlug('senior-hire-changes-everything-how-to-get-right');
-  console.log("HTML CONTENT:\n", post.content.substring(0, 500));
-}
-
-run().catch(console.error);
